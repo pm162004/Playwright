00e0ee24c667352f630031e83224b61a8e66d1d3
pm162004 <pm121614@gmail.com>
2025-04-30 16:52:01 +0530

    initial commit

--- PlayWright/playwrightBasics.py
+++ PlayWright/playwrightBasics.py
@@ -1,5 +1,25 @@
 # import pytest
-# from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page, sync_playwright
+def test_playwrightBasics(page: Page):
+    page.goto("https://www.google.com")
+
+def test_coreLocators(page: Page):
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.get_by_label("Username:").fill("rahulshettyacademy")
+    page.get_by_label("Password:").fill("learning")
+    page.get_by_role("button", name="Login").click()
+
+
+
+
+
+
+
+
+
+
+
+
 # def test_playwrightBasics():
 #     with sync_playwright() as p:
 #         browser = p.chromium.launch(headless=False)
@@ -8,9 +28,13 @@
 #         page.goto("https://www.google.com")
 #         page.screenshot(path="google.png")
 #         browser.close()
-def test_playwrightBasics(playwright):
-    browser = playwright.chromium.launch(headless=False)
-    browser.new_context()
-    page = browser.new_page()
-    page.goto("https://www.google.com")
-    browser.close()
+# def test_playwrightBasics(playwright):
+#     browser = playwright.chromium.launch(headless=False)
+#     browser.new_context()
+#     page = browser.new_page()
+#     page.goto("https://www.google.com")
+#     browser.close()
+
+
+# def test_playwrightBasics(page: Page):
+#     page.goto("https://www.google.com")
